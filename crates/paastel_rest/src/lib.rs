3e326e1cb4b971add5ac65f6cffad04911cd25fb
murilo ijanc <mbsd@m0x.ru>
2024-03-17 18:48:41 -0300

    chore: add graceful shutdown

--- crates/paastel_rest/src/lib.rs
+++ crates/paastel_rest/src/lib.rs
@@ -1,14 +1,80 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Copyright (c) 2024 Murilo Ijanc' <mbsd@m0x.ru>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+pub mod error;
+
+use std::time::Duration;
+
+use axum::response::IntoResponse;
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+use tokio::signal;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+
+pub async fn serve() -> Result<(), error::Error> {
+    // Create a regular axum app.
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/healthz", get(healthz))
+        .layer((
+            TraceLayer::new_for_http(),
+            // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
+            // requests don't hang forever.
+            TimeoutLayer::new(Duration::from_secs(10)),
+        ));
+
+    // Create a `TcpListener` using tokio.
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    // Run the server with graceful shutdown
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
+}
+
+async fn handler() -> impl IntoResponse {
+    Html("<h1>Hello, PaaStel!</h1>")
+}
+
+async fn healthz() -> impl IntoResponse {
+    "ok"
 }
